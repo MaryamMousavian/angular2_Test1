3c190f5b597eee24c2aa3a5eca97129eb00d0273
default <mousavian12@gmail.com>
2017-08-31 01:39:48 +0430

    InLine Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<h1> InLine Template!</h1>',
-  styleUrls: ['./app.component.css']
+  styles: ['h1{color: red}']
 })
 export class AppComponent {
   title = 'app';
