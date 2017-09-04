99232cb3c8e65da26499ce30d2095af213c7a42a
m.mousavian <mousavian12@gmail.com>
2017-09-04 14:44:47 +0430

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { OtherComponent } from './other/other.component';
 import { AnotherComponent } from './other/another.component';
 import { DatabindingComponent } from './other/databinding/databinding.component';
+import { PropertyBindingComponent } from './other/databinding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OtherComponent,
     AnotherComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule
