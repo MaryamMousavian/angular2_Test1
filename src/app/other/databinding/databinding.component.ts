a7f69d3ab4bab474222276fb294ebd6376ea2fed
m.mousavian <mousavian12@gmail.com>
2017-09-04 15:15:23 +0430

    end of property binding

--- src/app/other/databinding/databinding.component.ts
+++ src/app/other/databinding/databinding.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+// import {PropertyBindingComponent} from "./property-binding.component";
 
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
   styleUrls: ['./databinding.component.css']
+  // directives: [PropertyBindingComponent]
 })
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation!';
